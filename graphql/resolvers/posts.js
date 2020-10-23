d7cbf7e77470d5f5fcbae5a4de27105544c6050c
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-10-23 14:06:57 -0700

    compleets login/registration

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,96 +1,96 @@
 const { AuthenticationError, UserInputError } = require("apollo-server");
-const { argsToArgsConfig } = require("graphql/type/definition");
+// const { argsToArgsConfig } = require("graphql/type/definition");
 
-const Post = require('../../models/Post')
-const checkAuth = require('../../util/check-auth')
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find().sort({ createdAt: -1});  
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
-      } catch(err) {
+      } catch (err) {
         throw new Error(err);
       }
     },
     async getPost(_, { postId }) {
-      try{
+      try {
         const post = await Post.findById(postId);
-        if(post){
-          return post
+        if (post) {
+          return post;
         } else {
-          throw new Error('Post not found')
+          throw new Error("Post not found");
         }
-      } catch(err) {
+      } catch (err) {
         throw new Error(err);
       }
-    }
+    },
   },
 
   Mutation: {
-    async createPost(_, { body }, context){
+    async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      
-      if(args.body.trim() === ''){
-        throw new Error('Post body must not be empty')
+
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
       }
- 
+
       const newPost = new Post({
         body,
         user: user.id,
         userName: user.userName,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
       });
 
-      const post = await newPost.save()
+      const post = await newPost.save();
 
       context.pubsub.publish("NEW_POST", {
-        newPost: post
+        newPost: post,
       });
 
       return post;
     },
-    async deletePost(_, { postId }, context){
+    async deletePost(_, { postId }, context) {
       const user = checkAuth(context);
 
-      try{
+      try {
         const post = await Post.findById(postId);
-        if(user.userName === post.userName){
+        if (user.userName === post.userName) {
           await post.delete();
-          return 'Post deleted sucessfully';
+          return "Post deleted sucessfully";
         } else {
-          throw new AuthenticationError("Action not allowed")
+          throw new AuthenticationError("Action not allowed");
         }
-      } catch(err) {
-        throw new Error(err)
+      } catch (err) {
+        throw new Error(err);
       }
     },
     async likePost(_, { postId }, context) {
-     const { userName } = checkAuth(context);
+      const { userName } = checkAuth(context);
 
-     const post = await Post.findById(postId);
-     if(post){
-       if(post.likes.find(like => like.userName === userName)){
-        // Post already liked, unlike it
-        post.likes = post.likes.filter(like => like.userName !== userName)
-       } else {
-         // notliked, like post
-         post.likes.push({
-           userName,
-           createdAt: new Date().toISOString()
-         })
-       }
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.userName === userName)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.userName !== userName);
+        } else {
+          // notliked, like post
+          post.likes.push({
+            userName,
+            createdAt: new Date().toISOString(),
+          });
+        }
 
-       await post.save()
-       return post;
-     } else throw new UserInputError("Post not found")
-    }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
   },
 
   Subscription: {
     newPost: {
-      subscribe: (parent, _, { pubsub }) => pubsub.asyncIterator('NEW_POST')
-    }
-  }
-}
+      subscribe: (parent, _, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
+    },
+  },
+};
