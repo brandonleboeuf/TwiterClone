884be7840838b8950fc6751673a9c4ca4adb43ff
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-10-23 10:49:20 -0700

    compleets section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Grid } from "semantic-ui-react";
 
-const Home = () => {
+import PostCard from "../components/PostCard";
+
+function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  if (data) {
+    console.log(data);
+  }
   return (
-    <div>
-      <h1>
-        Home Page
-      </h1>
-    </div>
-  )
+    <Grid columns={3}>
+      <Grid.Row
+        className="page-title"
+        style={{
+          display: "block",
+          justifyContent: "center",
+          textAlign: "center",
+          fontSize: "2rem",
+          marginTop: "10px",
+        }}
+      >
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      {loading ? (
+        <h1>Loading posts...</h1>
+      ) : (
+        data &&
+        data.getPosts.map((post) => (
+          <Grid.Column key={post.id}>
+            <PostCard post={post} />
+          </Grid.Column>
+        ))
+      )}
+    </Grid>
+  );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      userName
+      likeCount
+      likes {
+        userName
+      }
+      commentCount
+      comments {
+        id
+        userName
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
