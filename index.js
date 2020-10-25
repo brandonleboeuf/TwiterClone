487d1db1742d9989741e687f5890d8a7b4fd9ebd
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-10-25 15:47:32 -0700

    fixes issues of deploy

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// dependency imports 
+// dependency imports
 const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 // relitave imports
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-const { MONGODB } = require('./config') // password and connection URL
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config'); // password and connection URL
 
 const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub })
-})
+  context: ({ req }) => ({ req, pubsub }),
+});
 
 // conects to server
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true  })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-      console.log("MongoDb Connected");
-    return server.listen({ port: 5000 });
+    console.log('MongoDb Connected');
+    return server.listen({ port: PORT });
   })
-  .then(res => {
-    console.log(`Server running at ${res.url}`)
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
   })
+  .catch((err) => {
+    console.error(err);
+  });
